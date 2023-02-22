c9ac93ff4e7231234d7399f256bf760d792e4bfe
Power-tile <hejizheng2004@163.com>
2023-02-21 18:55:37 -0600

    Initial springboot project

---
+++ backend/iplanner/src/test/java/edu/illinois/cs/iplanner/IplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.iplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
