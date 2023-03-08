f119ec35f66d7fe36ac6dc5017a182bac386a94a
EdwardHuang777 <zihaoh3@illinois.edu>
2023-03-07 20:58:29 -0600

    Complete part of api

--- backend/src/main/java/edu/illinois/cs/iplanner/controller/CourseController.java
+++ backend/src/main/java/edu/illinois/cs/iplanner/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.illinois.cs.iplanner.dao.CourseDAO;
@@ -15,8 +16,17 @@ public class CourseController {
     @Autowired
     CourseDAO courseDAO;
 
-    @RequestMapping("/")
-    List<CourseDTO> findAllCourses() {
-        return courseDAO.findAll();
-    }
+    // @RequestMapping("/")
+    // List<CourseDTO> findAllCourses() {
+    //     return courseDAO.findAll();
+    // }
+    @RequestMapping("/subjects")                            // Returns a list of subjects (e.g. CS, Stats, ECE, etc.)
+
+    @RequestMapping("/subjects/Computer_Science")           // Returns a list of courses in "Computer Science Category"
+
+    @RequestMapping("/subjects/Computer_Science/101")       // Returns the info of the course (e.g. CS 101: "title: Intro Computing: ...", "credit: 3", "prereq: if any", "concur: if any", "equiv: if any")
+
+    @RequestMapping("/search")                                  // Returns a list of courses that users intend to search
+    @ResponseBody
+    public List<CourseDTO> searchforcourse()
 }
