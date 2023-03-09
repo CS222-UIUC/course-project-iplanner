b76ff63cee06eecdbe021c1844f6006c4fb8a180
Power-tile <hejizheng2004@163.com>
2023-03-08 19:12:00 -0600

    First integration of planning & search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { Card, Grid, NextUIProvider } from '@nextui-org/react';
+
+import { ReactSortable } from "react-sortablejs";
+import SearchBar from './SearchBar';
+
+interface Course {
+  id: string,
+  subject: string,
+  number: string,
+  title: string
+}
+
 function App() {
+  const [courseList, setCourseList] = useState<Course[]>([
+    { id: "1", subject: "CS", number: "124", title: "Computer Science I"},
+    { id: "2", subject: "CS", number: "128", title: "Computer Science II"},
+  ]);
+
+  const [courseList2, setCourseList2] = useState<Course[]>([
+    { id: "3", subject: "CS", number: "225", title: "Data Structures"},
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider>
+      <Grid.Container>
+        <Grid xs={9}>
+          <Grid.Container>
+            <Grid xs={4}>
+              <ReactSortable list={courseList} setList={setCourseList} group="courses">
+                {courseList.map((item) => (
+                  <Card key={item.id}>{`${item.subject} ${item.number}`}<br/>{item.title}</Card>
+                ))}
+              </ReactSortable>
+            </Grid>
+            <Grid xs={4}>
+              <ReactSortable list={courseList2} setList={setCourseList2} group="courses">
+                {courseList2.map((item) => (
+                  <Card key={item.id}>{`${item.subject} ${item.number}`}<br/>{item.title}</Card>
+                ))}
+              </ReactSortable>
+            </Grid>
+          </Grid.Container>
+        </Grid>
+        <Grid xs={3}>
+          <SearchBar />
+        </Grid>
+      </Grid.Container>
+    </NextUIProvider>
   );
 }
 
