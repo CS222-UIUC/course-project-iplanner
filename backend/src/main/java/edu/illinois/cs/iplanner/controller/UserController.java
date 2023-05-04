76e2f638e93264ea2d0e0a812ffce94e77798086
Power-tile <hejizheng2004@163.com>
2023-05-04 15:36:28 -0500

    Login with custom password finished

--- backend/src/main/java/edu/illinois/cs/iplanner/controller/UserController.java
+++ backend/src/main/java/edu/illinois/cs/iplanner/controller/UserController.java
@@ -1,5 +1,6 @@
 package edu.illinois.cs.iplanner.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -9,22 +10,56 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.illinois.cs.iplanner.dao.SaveDAO;
+import edu.illinois.cs.iplanner.dao.UserDAO;
 import edu.illinois.cs.iplanner.model.SaveDTO;
+import edu.illinois.cs.iplanner.model.UserDTO;
+import edu.illinois.cs.iplanner.service.UserSessionService;
+import edu.illinois.cs.iplanner.vo.LoginVO;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    UserDAO userDAO;
 
     @Autowired
     SaveDAO saveDAO;
 
+    @Autowired
+    UserSessionService userSessionService;
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginVO loginVO, HttpSession session, HttpServletResponse response) throws IOException {
+        List<UserDTO> users = userDAO.findAllByUsernameAndPassword(loginVO.getUsername(), loginVO.getPassword());
+        if (users.isEmpty()) { // Auth failed
+            response.setStatus(401);
+            response.getWriter().append("401");
+            return;
+        }
+
+        session.setAttribute("user_id", users.get(0).getId());
+    }
+
+    @GetMapping("/debug/create")
+    public void create(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password) {
+        UserDTO user = new UserDTO();
+        user.setUsername(username);
+        user.setPassword(password);
+        userDAO.save(user);
+    }
+
     @GetMapping("/plan")
-    public List<List<String>> getUserPlan(@PathVariable(name = "userId") String userId) {
-        SaveDTO save = saveDAO.findByUserId(userId);
+    public List<List<String>> getUserPlan(HttpSession session) {
+        String userId = userSessionService.userId(session);
+        SaveDTO save = saveDAO.findByUser(userId);
         if (save == null) {
             return new ArrayList<>();
         }
@@ -32,8 +67,9 @@ public class UserController {
     }
 
     @PostMapping("/save")
-    public void saveUserPlan(@PathVariable(name = "userId") String userId, @PathVariable(name = "plan") List<List<String>> plan){
-        SaveDTO save = saveDAO.findByUserId(userId);
+    public void saveUserPlan(HttpSession session, @RequestBody List<List<String>> plan){
+        String userId = userSessionService.userId(session);
+        SaveDTO save = saveDAO.findByUser(userId);
         if (save != null) {
             save.setPlan(plan);
         }
--- backend/src/main/java/edu/illinois/cs/iplanner/dao/SaveDAO.java
+++ backend/src/main/java/edu/illinois/cs/iplanner/dao/SaveDAO.java
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import edu.illinois.cs.iplanner.model.SaveDTO;
 
 public interface SaveDAO extends MongoRepository<SaveDTO, String> {
-    SaveDTO findByUserId(String userId);
+    SaveDTO findByUser(String user);
 }
